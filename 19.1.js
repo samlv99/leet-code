ce71f193ebd1709eaa311532de7ef84d6dfb4405
samlv <samlv3112@gmail.com>
2023-04-12 00:24:36 +0700

    update

--- 19.1.js
+++ 19.1.js
@@ -1,6 +1,29 @@
 function removeNthFromEnd(head, n) {
-  
-
+  // Two pointers - fast and slow
+  let slow = head;
+  let fast = head;
+  // Move fast pointer n steps ahead
+  for (let i = 0; i < n; i++) {
+    if (fast.next === null) {
+      // If n is equal to the number of nodes, delete the head node
+      if (i === n - 1) {
+        head = head.next;
+      }
+      
+      return head;
+    }
+    fast = fast.next;
+  }
+  // Loop until we reach to the end.
+  // Now we will move both fast and slow pointers
+  while (fast.next !== null) {
+    slow = slow.next;
+    fast = fast.next;
+  }
+  // Delink the nth node from last
+  if (slow.next !== null) {
+    slow.next = slow.next.next;
+  }
   return head;
 }
 
